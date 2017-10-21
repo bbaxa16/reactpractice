8d51643dcaea298aa2565823a3a14edde404ee98
bbaxa16 <brooke769@gmail.com>
2017-10-21 13:21:36 -0600

    learning how to use props

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './App.js';
 
+const person = {
+  personName: 'Nick',
+  personAge: 88,
+  favorites: [
+    'lions',
+    'tigers',
+    'bears'
+  ]
+}
+
 ReactDOM.render(
-  <Hello />,
+  <Hello
+    name={person.personName}
+    age={person.personAge}
+    animals={person.favorites}
+  />,
   document.getElementById('root')
 )
